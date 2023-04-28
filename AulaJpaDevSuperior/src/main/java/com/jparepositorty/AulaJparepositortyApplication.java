97e51280639ea7f0f49097791d073ef3770810e4
Cercadu <Cadu-ce1@hotmail.com>
2023-04-28 08:40:08 -0300

    Aula JPA

---
+++ AulaJpaDevSuperior/src/main/java/com/jparepositorty/AulaJparepositortyApplication.java
@@ -0,0 +1,13 @@
+package com.jparepositorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJparepositortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJparepositortyApplication.class, args);
+	}
+
+}
---
+++ AulaJpaDevSuperior/src/test/java/com/jparepositorty/AulaJparepositortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jparepositorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJparepositortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
